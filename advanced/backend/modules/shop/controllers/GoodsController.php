c0e36029bc56ef560a0630ca7303f0dfd320d2b9
lvzhenchao <1370164516@qq.com>
2022-02-01 22:59:35 +0800

    Update GoodsController.php

--- advanced/backend/modules/shop/controllers/GoodsController.php
+++ advanced/backend/modules/shop/controllers/GoodsController.php
@@ -8,6 +8,8 @@ class GoodsController extends Controller {
 
     public function actionIndex()
     {
-        return '我是shop模块下的goods控制器下面的控制器方法index';
+//        return '我是shop模块下的goods控制器下面的控制器方法index';
+
+        return $this->render('index');
     }
 }
