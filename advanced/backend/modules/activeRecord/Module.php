f4b425660d18d26620c0c209aab20b12e53083db
lvzhenchao <1370164516@qq.com>
2022-02-10 22:37:10 +0800

    创建AR模块

---
+++ advanced/backend/modules/activeRecord/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace backend\modules\ActiveRecord;
+
+/**
+ * activeRecord module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'backend\modules\ActiveRecord\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
