a705b10689ac5644f9479fa16c3c0e9ac1abeb8a
lvzhenchao <1370164516@qq.com>
2022-02-20 21:52:23 +0800

    behavior行为的理解与使用

---
+++ advanced/backend/modules/behavior/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace backend\modules\behavior;
+
+/**
+ * behavior module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'backend\modules\behavior\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
