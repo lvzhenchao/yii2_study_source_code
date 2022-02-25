3d80f3a8a1f171789ecbaabd73b25b6c83ad989c
lvzhenchao <1370164516@qq.com>
2022-02-25 09:43:01 +0800

    $event形参是为传递参数给观察者

--- advanced/backend/modules/event/models/OLog.php
+++ advanced/backend/modules/event/models/OLog.php
@@ -2,6 +2,8 @@
 namespace backend\modules\event\models;
 
 class OLog {
+
+    //$event的形参，它会将本次事件一些必要的参数传递给每个观察者的方法
     public static function add($event){
         echo "我记录了一条登录记录";
     }
--- advanced/backend/modules/event/models/User.php
+++ advanced/backend/modules/event/models/User.php
@@ -2,6 +2,8 @@
 namespace backend\modules\event\models;
 
 class User {
+
+    //$event的形参，它会将本次事件一些必要的参数传递给每个观察者的方法
     public static function notifyFriend($event){
         echo "告诉了朋友们我登录了";
     }
