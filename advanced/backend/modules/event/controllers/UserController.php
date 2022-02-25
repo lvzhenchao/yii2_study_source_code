a36c2e42413e9028eb0bfdca69c038edca513ee5
lvzhenchao <1370164516@qq.com>
2022-02-25 21:43:58 +0800

    事件的简单示例

--- advanced/backend/modules/event/controllers/UserController.php
+++ advanced/backend/modules/event/controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace backend\modules\event\controllers;
 
+use backend\modules\event\events\UserLoginEvent;
 use yii\web\Controller;
 
 /**
@@ -9,12 +10,27 @@ use yii\web\Controller;
  */
 class UserController extends Controller
 {
-    /**
-     * Renders the index view for the module
-     * @return string
-     */
+
+    const EVENT_USER_LOGIN = 'user_login';
+
+    //事件绑定、观察者订阅主题
+    public function __construct()
+    {
+        //绑定事件
+        $this->on(self::EVENT_USER_LOGIN, ['backend\modules\event\models\OLog', 'add']);
+        $this->on(self::EVENT_USER_LOGIN, ['backend\modules\event\models\Admin', 'sendMail']);
+        $this->on(self::EVENT_USER_LOGIN, ['backend\modules\event\models\User', 'notifyFriend']);
+    }
+
     public function actionIndex()
     {
-        echo "登录";
+
+        echo "登录成功";
+        //触发事件
+        //如何确定给谁发短信呢
+        $event = new UserLoginEvent();
+        $event->userId = 111;
+
+        $this->trigger(self::EVENT_USER_LOGIN, $event);
     }
 }
--- advanced/backend/modules/event/models/User.php
+++ advanced/backend/modules/event/models/User.php
@@ -5,6 +5,7 @@ class User {
 
     //$event的形参，它会将本次事件一些必要的参数传递给每个观察者的方法
     public static function notifyFriend($event){
-        echo "告诉了朋友们我登录了";
+        $userID = $event->userId;
+        echo "告诉了朋友们我登录了=>".$userID;
     }
 }
