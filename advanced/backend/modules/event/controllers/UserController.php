32e8a7b6ce14f444b50566356b0c50beafb77a1e
lvzhenchao <1370164516@qq.com>
2022-02-25 09:38:07 +0800

    Create UserController.php

---
+++ advanced/backend/modules/event/controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace backend\modules\event\controllers;
+
+use yii\web\Controller;
+
+/**
+ * Default controller for the `event` module
+ */
+class UserController extends Controller
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        echo "登录";
+    }
+}
