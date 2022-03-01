744e4da6700b436b8690b0c60a158becacdc5657
lvzhenchao <1370164516@qq.com>
2022-03-01 20:16:42 +0800

    Update HelloBehavior.php

--- advanced/backend/modules/behavior/behaviors/HelloBehavior.php
+++ advanced/backend/modules/behavior/behaviors/HelloBehavior.php
@@ -2,7 +2,22 @@
 namespace backend\modules\behavior\behaviors;
 
 use yii\base\Behavior;
+use yii\db\ActiveRecord;
 
 class HelloBehavior extends Behavior {
     public $name = "abei2017";
+
+    //凡是新建会员的时候，都在其username加一个“+”号
+    public function events(){
+        return [
+            ActiveRecord::EVENT_BEFORE_INSERT => 'beforeInsert',
+        ];
+    }
+
+    public function beforeInsert(){
+        $owner = $this->owner;
+        if ($owner->getIsNewRecord()) {
+            $owner->username .= '+';
+        }
+    }
 }
