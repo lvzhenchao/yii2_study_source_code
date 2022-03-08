72f88247ef36bbb744881bf391b22ca6b73bc71a
lvzhenchao <1370164516@qq.com>
2022-03-08 10:52:10 +0800

    Update ForkController.php

--- advanced/backend/modules/swoole/controllers/ForkController.php
+++ advanced/backend/modules/swoole/controllers/ForkController.php
@@ -10,16 +10,31 @@ use yii\web\Controller;
 class ForkController extends Controller
 {
 
+    //PHP实现多进程
     public function actionFork()
     {
         $pid = pcntl_fork();
 
         if ($pid == -1) {
             die("could not fork");
-        } elseif($pid == 0) {
+        } elseif($pid == 0) {//在子进程内返回0
             prd("我是子进程id：".getmypid());
+        } else {//在父进程内，返回子进程号 大于0
+            prd("我也是子进程".$pid);
+        }
+    }
+
+    //进程间的的通信
+    public function actionCommun()
+    {
+        $a = "lzc";
+        $pid = pcntl_fork();
+
+        if ($pid > 0) {
+            $a .= "zhh";
+            prd($a);
         } else {
-            prd("我是父进程".$pid);
+            prd($a);
         }
     }
 }
