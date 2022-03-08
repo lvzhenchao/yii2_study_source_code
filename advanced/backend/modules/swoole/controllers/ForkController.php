0e28f1bd705729821126385c585211e2aa8d6804
lvzhenchao <1370164516@qq.com>
2022-03-08 10:34:04 +0800

    Update ForkController.php

--- advanced/backend/modules/swoole/controllers/ForkController.php
+++ advanced/backend/modules/swoole/controllers/ForkController.php
@@ -13,13 +13,13 @@ class ForkController extends Controller
     public function actionFork()
     {
         $pid = pcntl_fork();
-        
+
         if ($pid == -1) {
             die("could not fork");
         } elseif($pid == 0) {
             prd("我是子进程id：".getmypid());
         } else {
-            prd("我是父级进程".$pid);
+            prd("我是父进程".$pid);
         }
     }
 }
