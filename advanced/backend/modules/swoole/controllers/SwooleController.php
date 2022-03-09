92e66cf58340977032d02fc0955234c46ff30100
lvzhenchao <1370164516@qq.com>
2022-03-09 13:16:12 +0800

    Create SwooleController.php

---
+++ advanced/backend/modules/swoole/controllers/SwooleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace backend\modules\swoole\controllers;
+
+use yii\web\Controller;
+
+/**
+ * Default controller for the `swoole` module
+ */
+class SwooleController extends Controller
+{
+
+
+    public function actionFork()
+    {
+        
+    }
+
+
+}
