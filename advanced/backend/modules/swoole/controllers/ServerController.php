199a528351480d83e7460dacb3ecfef14b2b4258
lvzhenchao <1370164516@qq.com>
2022-03-10 17:39:58 +0800

    Update ServerController.php

---
+++ advanced/backend/modules/swoole/controllers/ServerController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace backend\modules\swoole\controllers;
+
+use yii\web\Controller;
+
+
+class ServerController extends Controller
+{
+
+    public function actionIndex()
+    {
+        $server = new swoole_server('0.0.0.0', 8888, SWOOLE_PROCESS, SWOOLE_SOCK_TCP);
+        $setting = [
+            'worker_num'=>6,
+            'reactor_num'=>4,
+            'task_worker_num'=>3,
+        ];
+
+        $server->set($setting);
+
+        $server->start();
+    }
+
+
+}
