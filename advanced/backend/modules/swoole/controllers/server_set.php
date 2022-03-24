290b880a21f212d8f230f2d1ca06e1dad2583d7d
lvzhenchao <1370164516@qq.com>
2022-03-24 22:35:06 +0800

    Update server_set.php

--- advanced/backend/modules/swoole/controllers/server_set.php
+++ advanced/backend/modules/swoole/controllers/server_set.php
@@ -4,11 +4,25 @@ $server = new Swoole\Server('0.0.0.0', 6666, SWOOLE_PROCESS, SWOOLE_SOCK_TCP);
 
 $server->on("receive", function ($params){//这个方法是TCP协议运行时必须有的
     //var_dump($params->setting);//另开一个xshell telnet 127.0.0.1 6666 随便输入点儿东西
-    print_r($params->master_pid);
-    print_r($params->manager_pid);
-    print_r($params->worker_id);//多个里面的其中一个
 
+//    var_dump($params->master_pid);
+//    var_dump($params->manager_pid);
+//    var_dump($params->worker_id);
+//    var_dump($params->worker_pid);//多个里面的其中一个
 
+//    foreach ($params->connections as $fd) {//文件描述符
+//        echo 'fd...'.$fd.PHP_EOL;
+//    }
+
+});
+
+$server->on("workerstart", function ($server){
+    //echo $server->worker_pid.PHP_EOL;//会打印出所有进程总和数量次数：worker_num + task_worker_num
+    if ($server->taskworker) {
+        echo "task_pid：".$server->worker_pid.PHP_EOL;
+    } else {
+        echo "worker_pid：".$server->worker_pid.PHP_EOL;
+    }
 
 });
 
@@ -23,7 +37,7 @@ $server->on("task", function (){
 
 });
 
-//netstat -nltp 端口号
+//netstat -nltp 查看端口号
 //pstree -p 26452 查看是否是6个worker进程
 // pstree -p 26823
 //php(26823)─┬─php(26826)─┬─php(26830)
@@ -50,6 +64,17 @@ $server->on("task", function (){
 //
 //$worker_pid
 //得到当前Worker进程的操作系统进程ID
+
+//$connections
+//TCP连接迭代器，可以使用foreach遍历服务器当前所有的连接，此属性的功能与Server->getClientList是一致的，但是更加友好。遍历的元素为单个连接的fd。
+//
+//$taskworker
+//
+//布尔类型
+//    true表示当前的进程是Task工作进程
+//    false表示当前的进程是Worker进程
+
+
 
 //telnet 127.0.0.1 6666 运行完之后再去输入随意字符；相当于去请求了
 
