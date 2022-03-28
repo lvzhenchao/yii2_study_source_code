64acf0d02de1ae9310084092b6e0b127be2cb950
lvzhenchao <1370164516@qq.com>
2022-03-28 22:48:59 +0800

    异步任务

--- advanced/backend/modules/swoole/controllers/server_function.php
+++ advanced/backend/modules/swoole/controllers/server_function.php
@@ -2,23 +2,24 @@
 
 $server = new Swoole\Server('0.0.0.0', 6666, SWOOLE_PROCESS, SWOOLE_SOCK_TCP);
 
-$server->on("receive", function ($params){//这个方法是TCP协议运行时必须有的
-
+$server->on("receive", function ($serv){//这个方法是TCP协议运行时必须有的
+    $serv->task("send mail");
+    echo "login...".PHP_EOL;
 });
 
 $server->on("start", function ($serv) {//最开始调用的回调方法
-    echo "master_pid：".$serv->master_pid.PHP_EOL;
-    echo "manager_pid：".$serv->manager_pid.PHP_EOL;
+//    echo "master_pid：".$serv->master_pid.PHP_EOL;
+//    echo "manager_pid：".$serv->manager_pid.PHP_EOL;
 });
 
 $server->on("workerstart", function ($serv, $worker_id) {//WorkerStart/onStart 是并发执行的，没有先后顺序
-    if ($serv->taskworker) {
-        echo "task_pid：".$serv->worker_pid.PHP_EOL;
-        echo "task_id......".$worker_id.PHP_EOL;
-    } else {
-        echo "worker_pid：".$serv->worker_pid.PHP_EOL;
-        echo "worker_id......".$worker_id.PHP_EOL;
-    }
+//    if ($serv->taskworker) {
+//        echo "task_pid：".$serv->worker_pid.PHP_EOL;
+//        echo "task_id......".$worker_id.PHP_EOL;
+//    } else {
+//        echo "worker_pid：".$serv->worker_pid.PHP_EOL;
+//        echo "worker_id......".$worker_id.PHP_EOL;
+//    }
 
 });
 
@@ -30,8 +31,10 @@ $server->set([
     "task_worker_num"=>3, //task进程数：务必要注册onTask || onFinish2个事件回调函数
 ]);
 
-$server->on("task", function (){
-
+$server->on("task", function ($serv,$task_id,$src_worker_id, $data){
+    echo "task....task_id:".$task_id.PHP_EOL;
+    echo "task....src_worker_id:".$src_worker_id.PHP_EOL;
+    echo "task....data:".$data.PHP_EOL;
 });
 
 
