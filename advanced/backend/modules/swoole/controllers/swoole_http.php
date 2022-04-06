ec7777c7730f35a6e941324b16bcf280171f5157
lvzhenchao <1370164516@qq.com>
2022-04-06 22:18:28 +0800

    http_response相关操作

--- advanced/backend/modules/swoole/controllers/swoole_http.php
+++ advanced/backend/modules/swoole/controllers/swoole_http.php
@@ -3,7 +3,15 @@
 $http = new Swoole\Http\Server('0.0.0.0', 6789);
 
 $http->on('request', function ($request, $response) {
-    $response->end("hello http");
+//    $response->end("hello end");//发送 Http 响应体，并结束请求处理
+//    print_r($request);
+//    print_r($request->get);
+
+    //$response->header('content-type', 'image/jpeg', true);
+//    $response->status('405', 'ss');
+//    $response->cookie('lzc', 'hh');
+    $response->write("hello write1");
+    $response->write("hello write2");
 });
 
 $http->start();
